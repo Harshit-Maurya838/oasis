523a36995cb765358fb325e559a32f60cfafd4f2
Harshit-Maurya838 <44harshitmaurya@gmail.com>
2025-01-15 10:38:30 +0530

    added: hamburger for navigation and made responsive and some tweaks

--- client/src/components/ui/hero.ui.component.jsx
+++ client/src/components/ui/hero.ui.component.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import heroImg from "/img/hero/hero.png";
+import hero2Img from "/img/hero/hero_2.png";
+import hero3Img from "/img/hero/hero_3.png";
 import '../../styles/utils/utils.styles.css';
-import "../../styles/hero/main.ui.styles.css";
+import "../../styles/hero/main.hero.styles.css";
 
 const HeroSection = () => {
   return (
@@ -12,7 +14,11 @@ const HeroSection = () => {
             <p className='text-20-regular'>Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for <br />ultimate relaxation</p>
         </div>
         <div className="container">
-            <img src={heroImg} alt="" />
+            <picture>
+                <source srcSet={hero3Img} media="(max-width: 393px)" />
+                <source srcSet={hero2Img} media="(min-width: 393px) and (max-width: 834px)" />
+                <img src={heroImg} alt="Furniture Store" />
+            </picture>
         </div>
     </section>
   )
---
+++ client/src/components/utils/hamburger.utils.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../../styles/utils/hamburger.utils.styles.css'
+
+const Hamburger = () => {
+  return (
+
+  )
+}
+
+export default Hamburger
