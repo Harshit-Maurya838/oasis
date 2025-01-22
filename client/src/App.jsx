36742d1c93fd55f8e7c5ae560453128c772b1e56
Nikh-on-Linux <nikhilhegde1011@gmail.com>
2025-01-23 00:51:06 +0530

    add: item card component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,48 @@
-import Navbar from './components/ui/navbar.ui.component'
-import './App.css'
-import HeroSection from './components/ui/hero.ui.component'
-import oasisLogo from '/Oasis.svg'
-import Footer from './components/ui/footer.ui.component'
-import Categories from './components/ui/categories.ui.component'
-import FaQ from './components/ui/faq.ui.component'
-import DropDown from './components/utils/dropdown.utils.component'
-import Product from './components/utils/productCard.utils.component'
-import StarRating from './components/utils/rating.utils.component'
+import Navbar from "./components/ui/navbar.ui.component";
+import "./App.css";
+import HeroSection from "./components/ui/hero.ui.component";
+import oasisLogo from "/Oasis.svg";
+import Footer from "./components/ui/footer.ui.component";
+import Categories from "./components/ui/categories.ui.component";
+import FaQ from "./components/ui/faq.ui.component";
+import DropDown from "./components/utils/dropdown.utils.component";
+import Product from "./components/utils/productCard.utils.component";
+import StarRating from "./components/utils/rating.utils.component";
 
 function App() {
-
+  let data = {
+    title: "Faux Leather Sofa Couch new premium",
+    category: "Chair",
+    tags: "",
+    desc: "AMAZING CHAIR",
+    basePrice: "500",
+    rating: "4.2",
+    variants: [
+      {
+        var_name: "name of the variant",
+        var_url: "endpoints of the url",
+        var_gallery: ["array of image gallery"],
+        var_color: "red",
+        discount: "40%",
+      },
+      {
+        var_name: "name of the variant",
+        var_url: "endpoints of the url",
+        var_gallery: ["array of image gallery"],
+        var_color: "blue",
+        discount: "40%",
+      },
+    ],
+  };
 
   return (
     <>
-        <Navbar />
-        <section id="main-body">
-            <HeroSection />
-            <Categories />
-            <FaQ />
-            <DropDown items={[
+      <Navbar />
+      <section id="main-body">
+        <HeroSection />
+        <Categories />
+        <FaQ />
+        {/* <DropDown items={[
               {
                 'itemname':'Higher to lower',
                 'itemcallback':()=>{return(null)},
@@ -32,13 +55,20 @@ function App() {
                 'itemname':'Recently added',
                 'itemcallback':()=>{return(null)},
               }
-            ]} />
-            {/* <Product /> */}
-            <StarRating rating={3.5} />
-        </section>
-        <Footer />
+            ]} /> */}
+        <Product
+          imgSrc={"./img/samples/sample-image.png"}
+          productName={data.title}
+          price={data.basePrice}
+          variants={data.variants}
+          rating={data.rating}
+        />
+
+        {/* <StarRating rating={3.5} /> */}
+      </section>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- client/src/components/utils/productCard.utils.component.jsx
+++ client/src/components/utils/productCard.utils.component.jsx
@@ -1,21 +1,45 @@
-import React from 'react'
-import '../../styles/utils/productCard.utils.styles.css'
+import React from "react";
+import "../../styles/utils/productCard.utils.styles.css";
+import "../../styles/utils/utils.styles.css";
+import Button from "./button.utils.component";
 
-const Product = ({productName, price, imgSrc, variants}) => {
+const Product = ({ productName, price, imgSrc, variants , rating = 5 }) => {
   return (
-    <div>
+    <div className="cardDom">
+      <div className="cardImgDom">
         <img src={imgSrc} alt="Product" />
-        <div className="product_desc">
-            <p>{productName}</p>
-            <span>{price}</span>
+        <div className="button">
+          <Button>Add to cart</Button>
         </div>
-        <div className="variants">
-            {variants.map((variant, index) => (
-                <span key={index} className='variant_circle' style={{backgroundColor: variant.var_color}}></span>
-            ))}
+        <div className="rating">
+          <svg
+            key={`filled-${0}`}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            fill="#fec200"
+            width="18"
+            height="18"
+          >
+            <path d="M12 .587l3.668 7.431 8.2 1.196-5.934 5.782 1.4 8.164L12 18.896l-7.334 3.864 1.4-8.164L.133 9.214l8.2-1.196z" />
+          </svg>
+          <span className="text-13-regular" >{rating}</span>
         </div>
+      </div>
+      <div className="product_desc">
+        <p className="text-18-semibold" >{productName}</p>
+        <span className="text-13-semibold" >${price}</span>
+      </div>
+      <div className="variants">
+        {variants.map((variant, index) => (
+          <div
+            key={index}
+            className={`variant_circle ${index == 0 ? "active" : ""}`}
+            style={{ backgroundColor: variant.var_color }}
+          ></div>
+        ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Product
+export default Product;
