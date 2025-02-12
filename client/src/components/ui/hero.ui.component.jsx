c998e6d2f52fe18f9558ebb9b5a307abb61536b8
Nikh-on-Linux <nikhilhegde1011@gmail.com>
2025-02-12 13:52:38 +0530

    add:animations

--- client/src/components/ui/hero.ui.component.jsx
+++ client/src/components/ui/hero.ui.component.jsx
@@ -4,13 +4,27 @@ import hero2Img from "/img/hero/hero_2.png";
 import hero3Img from "/img/hero/hero_3.png";
 import '../../styles/utils/utils.styles.css';
 import "../../styles/hero/main.hero.styles.css";
+import "../../styles/utils/animations.utils.styles.css";
+import SplitText from "../animatedUtils/splitText.animatedUtils.component";
 
 const HeroSection = () => {
   return (
     <section id="hero">
         <div className='hero_text'>
-            <h1 className='text-18-semibold'>FURNITURE STORE</h1>
-            <p className='heading-02'>Discover the Artistry of Modern <br></br> Contemporary Furniture</p>
+            <h1 className='text-18-semibold fadein'>FURNITURE STORE</h1>
+            {/* <p className='heading-02'>Discover the Artistry of Modern <br></br> Contemporary Furniture</p> */}
+            <SplitText 
+              text='Discover the Artistry of Modern'
+              className='heading-02'
+              delay={25}
+              rootMargin='10px'
+            />
+            <SplitText 
+              text='Contemporary Furniture'
+              className='heading-02'
+              delay={50}
+
+            />
             <p className='text-20-regular'>Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for <br />ultimate relaxation</p>
         </div>
         <div className="container">
