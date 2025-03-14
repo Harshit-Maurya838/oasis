c0dde611282ee2c0c9c25d4d62581a24814cffa3
Nikh-on-Linux <nikhilhegde1011@gmail.com>
2025-03-14 19:34:41 +0530

    add:adding few anims

--- client/src/components/ui/hero.ui.component.jsx
+++ client/src/components/ui/hero.ui.component.jsx
@@ -11,7 +11,7 @@ const HeroSection = () => {
   return (
     <section id="hero">
         <div className='hero_text'>
-            <h1 className='text-18-semibold fadein'>FURNITURE STORE</h1>
+            <h1 className='text-18-semibold spreadIn'>FURNITURE STORE</h1>
             {/* <p className='heading-02'>Discover the Artistry of Modern <br></br> Contemporary Furniture</p> */}
             <SplitText 
               text='Discover the Artistry of Modern'
@@ -25,9 +25,9 @@ const HeroSection = () => {
               delay={50}
 
             />
-            <p className='text-20-regular'>Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for <br />ultimate relaxation</p>
+            <p className='text-20-regular fadein'>Experience the elegance and functionality of cutting-edge design where luxury meets innovation in every piece for <br />ultimate relaxation</p>
         </div>
-        <div className="container">
+        <div className="container fadein">
             <picture>
                 <source srcSet={hero3Img} media="(max-width: 393px)" />
                 <source srcSet={hero2Img} media="(min-width: 393px) and (max-width: 834px)" />
